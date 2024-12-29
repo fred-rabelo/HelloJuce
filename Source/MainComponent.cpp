428d1d649f4f9b309ecafa62f215e393ba3a9e7f
Fred Rabelo <fred.rabel@outlook.com>
2024-12-29 21:10:45 +0100

    Improved Graphs

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,8 +16,8 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::FontOptions (32.0f));
-    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (52.0f));
+    g.setColour (juce::Colours::yellow);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
